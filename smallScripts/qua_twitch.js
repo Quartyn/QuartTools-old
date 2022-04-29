61cdaabc498f140a45be07c6f8c74253323ed4a5
Quartyn <twentyvec@gmail.com>
2022-04-29 17:48:16 +0200

    Add files via upload

---
+++ smallScripts/qua_twitch.js
@@ -0,0 +1,13 @@
+﻿const PointsWait = 100;
+const CollectPoints = 6000;
+let Selector = '[data-test-selector="community-points-summary"] button.gjXDMG'; // 22.4.2022
+setInterval(() => {
+    let bonusChest = document.querySelector(Selector);
+    if (bonusChest) {
+        setTimeout(() => {
+            bonusChest.click();
+            console.log('%cQuartTools [%cqua.points%c]: %cPoints were collected.', 'color:cyan;', 'color:white;', 'color:cyan', 'color:white;');
+        }, PointsWait);
+    }
+}, CollectPoints);
+console.log('%cQuartTools [%cqua_twitch%c]: %cAutoclaim Bonus Points Activated!', 'color:cyan;', 'color:white;', 'color:cyan', 'color:white;');
